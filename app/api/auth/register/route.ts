a7b6303443076cbd62be66ef40755267ddb8eaf5
cliff <clifflee1022@gmail.com>
2024-05-02 10:18:59 +0800

    feat: login page ok

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -19,6 +19,7 @@ export async function POST(request: Request) {
   } catch (e: any) {
     console.log(e);
     const code = e.errors[0].extensions.code
+    console.log('---e.errors[0].extensions:', e.errors[0].extensions);
     if (code === 'RECORD_NOT_UNIQUE') {
       return NextResponse.json({ message: "This user already exist" }, { status: 409 });
     }
